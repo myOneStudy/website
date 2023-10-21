8b2fe530b078c48eda8e1b51b6453b9a914ec2d0
lipman <96100537+lipmaneth@users.noreply.github.com>
2023-10-21 07:46:21 -0500

    adds messaging for creating decks for free

--- tailwind.config.js
+++ tailwind.config.js
@@ -73,6 +73,7 @@ module.exports = {
         inter: ['var(--font-inter)', 'sans-serif'],
       },
       fontSize: {
+        xxs: '0.625rem',
         xs: '0.75rem',
         sm: '0.875rem',
         base: '1rem',
