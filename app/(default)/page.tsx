ec22acb92d609948ccd520ed90f313a6522eb4f2
marshall lipman <marshalllipman@marshalls-MBP.attlocal.net>
2023-10-29 21:28:22 -0500

    adds dynamic section. Revamps homepage

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,20 +1,23 @@
 export const metadata = {
   title: 'OneStudy.io',
-  description: 'Page description',
+  description: 'A study tool you will actually love using.',
 }
 
-import Hero from '@/components/hero'
+import Hero from '@/components/sb-hero'
 import Features from '@/components/features'
 import FeaturesBlocks from '@/components/features-blocks'
 import Testimonials from '@/components/testimonials'
 import Newsletter from '@/components/newsletter'
 import Accordion from '@/components/accordion'
+import Zigzag from '@/components/sb-zigzag'
+import Recall from '@/components/recall'
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Features />
+      <Zigzag/>
+      <Recall/>
       <FeaturesBlocks />      
       <Newsletter />
       <Accordion />
